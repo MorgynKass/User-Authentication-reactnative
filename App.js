75716d7408cd24d956a34950139eb112b8ebb0ac
MorgynKass <morgynkass@gmail.com>
2024-08-27 13:01:28 -0600

    setting up base code

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { Colors } from "./constants/Colors";
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.orange },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.lightBeige },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignUpScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.orange },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.lightBeige },
+      }}
+    >
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Navigation />
+    </>
+  );
+}
---
+++ constants/Colors.js
@@ -0,0 +1,7 @@
+export const Colors = {
+  orange: "#C5705D",
+  beige: "#DFD3C3",
+  darkBeige: "#D0B8A8",
+  lightBeige: "#F8EDE3",
+  error: "#fc1303",
+};
